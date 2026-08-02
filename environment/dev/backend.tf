3efb6613616ceb5ce9cb9a6e376c1b3563d7f9f7
Sanjeev Singh <sanjeevsinghred@gmail.com>
2026-08-02 11:45:05 +0530

    Skeleton for Child Modules and Environment/Dev Created

---
+++ environment/dev/backend.tf
@@ -0,0 +1,8 @@
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "rgforstate"
+#     storage_account_name = "astorageforstate"
+#     container_name       = "containerforstate"
+#     key                  = "statefilename.tfstate"
+#   }
+# }
