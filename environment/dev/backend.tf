d2e4ec85a8a742a8801a10dfc335838bc2639702
Sanjeev Singh <sanjeevsinghred@gmail.com>
2026-08-03 21:26:56 +0530

    added backend for state

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rgforstate"
-#     storage_account_name = "astorageforstate"
-#     container_name       = "containerforstate"
-#     key                  = "statefilename.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rgforstate"
+    storage_account_name = "astorageforstate"
+    container_name       = "containerforstate"
+    key                  = "statefilename.tfstate"
+  }
+}
--- modules/azurerm_nsg_subnet_association/main.tf
+++ modules/azurerm_nsg_subnet_association/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet_network_security_group_association" "this" {
-    for_each = var.config
+  for_each                  = var.config
   subnet_id                 = data.azurerm_subnet.this[each.key].id
   network_security_group_id = data.azurerm_network_security_group.this[each.key].id
 }
